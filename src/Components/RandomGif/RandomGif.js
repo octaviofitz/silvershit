abfbac46cf05e442ace4ea10a0252fc10fd52355
octaviofitz <octaviofitz@gmail.com>
2024-07-10 09:52:18 -0300

    change gif

--- src/Components/RandomGif/RandomGif.js
+++ src/Components/RandomGif/RandomGif.js
@@ -44,7 +44,7 @@ function RandonGif() {
             <button onClick={getNewGifs} className='boton'>QUIERO OTROS</button>
 
 
-            <div style="width:100%;height:0;padding-bottom:75%;position:relative;"><iframe src="https://giphy.com/embed/Rz9wM2XlX2eP2Zm3HY" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/sesamestreet-sesame-street-cookie-monster-Rz9wM2XlX2eP2Zm3HY">via GIPHY</a></p>
+         {/*    <div style="width:100%;height:0;padding-bottom:75%;position:relative;"><iframe src="https://giphy.com/embed/Rz9wM2XlX2eP2Zm3HY" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/sesamestreet-sesame-street-cookie-monster-Rz9wM2XlX2eP2Zm3HY">via GIPHY</a></p> */}
 
         </section>
     );
